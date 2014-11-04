df9c07c16fcb211d41d5f69b5d9ce6c3e6d78a6b
isms <isaac.slavitt@gmail.com>
2014-11-03 22:38:08 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='drivendata-submission-validator',
+    version='1.0',
+    description='DrivenData Submission Validator',
+    author='DrivenData, Inc.',
+    author_email='info@drivendata.org',
+    url='http://www.drivendata.org/',
+    license='MIT',
+)
